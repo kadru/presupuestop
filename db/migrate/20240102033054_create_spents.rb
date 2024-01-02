37fb76ff6b9940bfd5a3bc04f53a0ec5adabb38d
Oscar <orivas155@gmail.com>
2024-01-02 10:25:14 -0600

    refactor: change spent amount from integer to bigint

--- db/migrate/20240102033054_create_spents.rb
+++ db/migrate/20240102033054_create_spents.rb
@@ -1,10 +1,11 @@
 class CreateSpents < ActiveRecord::Migration[7.1]
   def change
     create_table :spents do |t|
-      t.integer :amount
+      t.bigint :amount
       t.string :name
       t.references :category
       t.references :subcategory
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_02_033054) do
   enable_extension "plpgsql"
 
   create_table "spents", force: :cascade do |t|
-    t.integer "amount"
+    t.bigint "amount"
     t.string "name"
     t.bigint "category_id"
     t.bigint "subcategory_id"
