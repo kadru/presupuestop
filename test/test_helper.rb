10169a6c8ec37e6cff3d9c41617e9232550ad88a
Oscar <orivas155@gmail.com>
2024-01-07 22:04:14 -0600

    test: add test config

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
@@ -10,6 +12,16 @@ module ActiveSupport
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
 
+    class << self
+      alias describe context
+    end
     # Add more helper methods to be used by all tests here...
+  end
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :minitest
+    with.library :rails
   end
 end
