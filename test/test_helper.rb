5423afd6eb5f6dc74fb533c57e3ab6524bb3aa1e
Oscar <orivas155@gmail.com>
2024-01-07 23:28:30 -0600

    test: change parallel strategy from workers to threads

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,7 +8,7 @@ module ActiveSupport
   class TestCase
     include FactoryBot::Syntax::Methods
     # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
+    parallelize(workers: :number_of_processors, with: :threads)
 
     class << self
       alias describe context
