11e66fac1a7cf700de5988fc0eb8fb7b81704542
Oscar <orivas155@gmail.com>
2024-02-04 18:14:51 -0600

    fix: set default HOST for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = {
-    host: ENV.fetch("HOST"),
+    host: ENV.fetch("HOST", "localhost"),
     port: ENV.fetch("PORT", 3000).to_i
   }
 
