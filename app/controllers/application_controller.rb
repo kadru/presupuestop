5dedbd9e79769840115b3acc4cc34449e8fc9610
Oscar <orivas155@gmail.com>
2024-02-04 20:12:38 -0600

    feat: change pasword

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
+# Main application controller, define here methods to inherit to all sub controllers
 class ApplicationController < ActionController::Base
+  private
+
+  def current_account
+    rodauth.rails_account
+  end
+
+  def authenticate
+    rodauth.require_account # redirect to login page if not authenticated
+  end
 end
