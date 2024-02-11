a87ee95185a6739f673e4d0f74b85ecb4657de46
Oscar <orivas155@gmail.com>
2024-02-10 23:18:16 -0600

    refactor: remove unused method Category.for_select

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,10 +2,6 @@
 
 # Gives to spents a custom category to belong
 class Category < ApplicationRecord
-  def self.for_select
-    pluck(:name, :id)
-  end
-
   belongs_to :account
   has_many :subcategories, dependent: :destroy
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -7,17 +7,4 @@ class CategoryTest < ActiveSupport::TestCase
     should have_many(:subcategories)
     should belong_to(:account)
   end
-
-  describe ".for_select" do
-    should "returns an array of categories name and id" do
-      category = create(:category)
-
-      assert_equal [
-        [
-          category.name,
-          category.id
-        ]
-      ], Category.for_select
-    end
-  end
 end
