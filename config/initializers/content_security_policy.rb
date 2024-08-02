56b9feca027b3dbf13994a94847560f36bc6fd45
Oscar <orivas155@gmail.com>
2024-08-01 19:51:54 -0600

    refactor: setup vite_rails for js only

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -13,7 +13,34 @@ Rails.application.configure do
     policy.img_src     :self, :https, :data
     policy.object_src  :none
     policy.script_src  :self, :https
-    policy.style_src   :self, :https
+
+    # Allow @vite/client to hot reload javascript changes in development
+    if Rails.env.development?
+      policy.script_src(
+        *policy.script_src,
+        :unsafe_eval,
+        "http://#{ViteRuby.config.host_with_port}")
+    end
+
+    # You may need to enable this in production as well depending on your setup.
+    policy.script_src(*policy.script_src, :blob) if Rails.env.test?
+
+    # Allow @vite/client to hot reload changes in development
+    policy.connect_src(*policy.connect_src, "ws://#{ViteRuby.config.host_with_port}") if Rails.env.development?
+    if Rails.env.development?
+      host = Rails.configuration.action_mailer.default_url_options.fetch(:host)
+      port = Rails.configuration.action_mailer.default_url_options.fetch(:port)
+      policy.connect_src(
+        *policy.connect_src,
+        "#{host}:#{port}"
+      )
+    end
+
+    policy.style_src :self, :https
+
+    # Allow @vite/client to hot reload style changes in development
+    policy.style_src(*policy.style_src, :unsafe_inline) if Rails.env.development?
+
     # Specify URI for violation reports
     # policy.report_uri "/csp-violation-report-endpoint"
   end
