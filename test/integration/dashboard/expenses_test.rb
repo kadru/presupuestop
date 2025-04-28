d535e300e1247178b8869a953a18e6320daa5be9
Oscar Rivas <orivas155@gmail.com>
2025-04-28 07:55:55 -0600

    feat: improve expenses dashboard
     The chart is improved by splitting it into one for categories and another for subcategories.

--- test/integration/dashboard/expenses_test.rb
+++ test/integration/dashboard/expenses_test.rb
@@ -4,47 +4,71 @@ require "test_helper"
 
 module Dashboard
   class ExpensesTest < ActionDispatch::IntegrationTest
-    test "render json grouped expenses for dashboard chart" do
-      account = create(:account, :with_expenses)
-      category = account.categories.last
-      subcategory = create(
-        :subcategory,
-        name: "mantenimiento",
-        category:
-      )
-      create(:expense, subcategory:, category:, account:)
-      category_feeding = create(:category, name: "alimentacion", account:)
-      subcategory_pantry = create(:subcategory, name: "despensa", category: category_feeding)
-      create(:expense, subcategory: subcategory_pantry, category: category_feeding, account:)
+    describe "GET /dashboard/expenses/amount_by_category" do
+      it "renders json grouped expenses sum by category" do
+        account = create(:account, :with_expenses)
+        category = account.categories.last
+        subcategory = create(
+          :subcategory,
+          name: "mantenimiento",
+          category:
+        )
+        create(:expense, subcategory:, category:, account:)
+        category_feeding = create(:category, name: "alimentacion", account:)
+        subcategory_pantry = create(:subcategory, name: "despensa", category: category_feeding)
+        create(:expense, subcategory: subcategory_pantry, category: category_feeding, account:)
 
-      login email: account.email, password: account.password
-      get "/dashboard/expenses", as: :json
+        login email: account.email, password: account.password
+        get "/dashboard/expenses/amount_by_category", as: :json
 
-      assert_response :success
-      assert_equal [
-        {
-          "name" => "alimentacion",
-          "children" => [
-            {
-              "name" => "despensa",
-              "value" => 100
-            }
-          ]
-        },
-        {
-          "name" => "vivienda",
-          "children" => [
-            {
-              "name" => "mantenimiento",
-              "value" => 100
-            },
-            {
-              "name" => "renta",
-              "value" => 100
-            }
-          ]
-        }
-      ], response.parsed_body
+        assert_response :success
+        assert_pattern do
+          response.parsed_body => [
+              {
+                groupId: Integer,
+                name: "vivienda",
+                value: 20_000
+              },
+              {
+                groupId: Integer,
+                name: "alimentacion",
+                value: 10_000
+              }
+            ]
+        end
+      end
+    end
+
+    describe "GET /dashboard/expenses/:id/amount_by_subcategory" do
+      it "renders json grouped expenses sum by subcategory" do
+        account = create(:account, :with_expenses)
+        category = account.categories.last
+        subcategory = create(
+          :subcategory,
+          name: "mantenimiento",
+          category:
+        )
+        create(:expense, subcategory:, category:, account:)
+
+        login email: account.email, password: account.password
+        get "/dashboard/expenses/#{category.id}/amount_by_subcategory", as: :json
+
+        assert_response :success
+        assert_pattern do
+          response.parsed_body => [
+              {
+                groupId: Integer,
+                name: "renta",
+                value: 10_000
+              },
+              {
+                groupId: Integer,
+                name: "mantenimiento",
+                value: 10_000
+              }
+            ]
+        end
+      end
     end
 
     private
